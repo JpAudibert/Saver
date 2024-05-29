ca712f9502e435c058b71ac626272c2b628dc1cf
João Pedro Basso Audibert <hotsjoao@gmail.com>
2024-05-28 22:55:21 -0300

    Mongo DB connection

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+const string connectionUri = "mongodb+srv://saver:#Savermanager123@saverdb.hkum5ju.mongodb.net/?retryWrites=true&w=majority&appName=SaverDB";
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +14,23 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Set the ServerApi field of the settings object to set the version of the Stable API on the client
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+// Create a new client and connect to the server
+var client = new MongoClient(settings);
+
+// Send a ping to confirm a successful connection
+try
+{
+    var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+    Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
