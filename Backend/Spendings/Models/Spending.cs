cf88d8569898509b965375a3d5e2f34350c69311
João Pedro Basso Audibert <hotsjoao@gmail.com>
2024-06-16 18:21:56 -0300

    Fixes spending class

--- Backend/Spendings/Models/Spending.cs
+++ Backend/Spendings/Models/Spending.cs
@@ -4,21 +4,8 @@ namespace Backend.Spendings.Models;
 
 public class Spending
 {
-    public ObjectId Id { get; set; }
-    public double Amount { get; set; }
-    public string Description { get; set; }
-    public string Type { get; set; }
-
-    public Spending()
-    {
-        Id = ObjectId.GenerateNewId();
-    }
-
-    public Spending(double amount, string description, string type)
-    {
-        Id = ObjectId.GenerateNewId();
-        Amount = amount;
-        Description = description;
-        Type = type;
-    }
+    public ObjectId Id { get; set; } = default!;
+    public double Amount { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string Type { get; set; } = default!;
 }
