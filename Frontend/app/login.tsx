dd9a45792c26f2f94307e7548e4bf81aa26ae926
Andres Boscaini <andres_bosca@hotmail.com>
2024-06-20 23:46:37 -0300

    ✨ introduce log in route

--- Frontend/app/login.tsx
+++ Frontend/app/login.tsx
@@ -7,25 +7,20 @@ import {
 } from '@/components/Layout/styles';
 import { LoginActionsContainer } from '@/components/LoginActions/styles';
 import { useRouter } from 'expo-router';
-import { View } from 'react-native';
+import { Pressable, View } from 'react-native';
 
 export default function Index() {
   const router = useRouter();
   return (
     <View style={{ flex: 1 }}>
       <HeaderContainer>
-        <HeaderText>SAVER</HeaderText>
-        <SloganText>Saving your money is our goal</SloganText>
+        <Pressable onPress={() => router.push('/')}>{'<- back'}</Pressable>
+        <SloganText>Log In</SloganText>
       </HeaderContainer>
 
       <LoginLayout>
         <LoginActionsContainer>
-          <Button title="Log In" onPress={() => router.navigate('login')} />
-          <Button
-            title="Sign Up"
-            onPress={() => console.log('sign up')}
-            fill={false}
-          />
+          <Button title="Log In" onPress={() => console.log('login')} />
         </LoginActionsContainer>
       </LoginLayout>
     </View>
