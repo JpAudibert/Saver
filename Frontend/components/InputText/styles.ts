d8e4de190ff67d8ef14768077d13b257c5f5ab02
João Pedro Basso Audibert <hotsjoao@gmail.com>
2024-07-01 23:23:10 -0300

    Fixes input style

--- Frontend/components/InputText/styles.ts
+++ Frontend/components/InputText/styles.ts
@@ -7,33 +7,20 @@ interface ContainerProps {
 
 const Container = styled.View<ContainerProps>`
   width: 280px;
-  height: 30px;
+  height: 40px;
   padding: 0 16px;
-  background: #d3b88c;
+  background: #F6FCF6;
   border-radius: 10px;
-  margin-bottom: 8px;
+  margin-bottom: 16px;
   border-width: 2px;
-  border-color: #846c5b;
-
   flex-direction: row;
   align-items: center;
-
-  ${(props) =>
-    props.isErrored &&
-    css`
-      border-color: #c53030;
-    `}
-
-  ${(props) =>
-    props.isFocused &&
-    css`
-      border-color: #c8ab83;
-    `}
+  border-color: #235789;
 `;
 
 const TextInput = styled.TextInput`
   flex: 1;
-  color: #232129;
+  color: #235789;
   font-size: 16px;
   /* font-family: 'RobotoSlab-Regular'; */
 `;
